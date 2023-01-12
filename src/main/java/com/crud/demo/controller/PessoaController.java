1505065907e34941f63a2d2dc22ee35381eb4a31
vanessa.bonifacio <vanessa1302@gmail.com>
2023-01-11 23:35:58 -0300

    funcionalidades solicitadas do desafio

--- src/main/java/com/crud/demo/controller/PessoaController.java
+++ src/main/java/com/crud/demo/controller/PessoaController.java
@@ -1,10 +1,13 @@
 package com.crud.demo.controller;
 
+import com.crud.demo.dto.EnderecoDTO;
 import com.crud.demo.dto.PessoaCadastroDTO;
 import com.crud.demo.dto.PessoaDTO;
 import com.crud.demo.service.PessoaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,9 +28,41 @@ public class PessoaController {
         return ResponseEntity.ok(service.obterPorId(id));
     }
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Page<PessoaDTO>> listar(Pageable pageable) {
+        log.info("listando pessoas");
+        return ResponseEntity.ok(service.listar(pageable));
+    }
+
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PessoaDTO> cadastrar(@Valid @RequestBody PessoaCadastroDTO dto) {
         log.info("Salvando Nova Pessoa");
         return ResponseEntity.ok(service.cadastrar(dto));
+    }
+
+    @PostMapping(path = "/{idPessoa}/adiciona-endereco", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PessoaDTO> adicionaEndereco(@Valid @RequestBody EnderecoDTO dto, @PathVariable Integer idPessoa ) {
+        log.info("Adicionando novo endereço a pessoa com id {}", idPessoa);
+        return ResponseEntity.ok(service.adicionarEndereco(idPessoa, dto));
+    }
+
+    @PatchMapping(path = "/{idPessoa}/alterar-status-endereco")
+    public ResponseEntity tornarPrincipal(@PathVariable Integer idPessoa, @RequestParam Integer idEndereco) {
+        log.info("Alterando para PRINCIPAL endereço com id {}", idEndereco);
+        service.mudarStatusEndereco(idPessoa, idEndereco);
+        return ResponseEntity.ok().body("Endereço alterado para principal com Sucesso!");
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PessoaDTO> editar(@Valid @RequestBody PessoaDTO dto) {
+        log.info("Atualizando Pessoa com id {} ", dto.getId());
+        return ResponseEntity.ok(service.atualizar(dto));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity excluir(@PathVariable Integer id) {
+        log.info("excluindo pessoa com id {}", id);
+        service.excluirPorId(id);
+        return ResponseEntity.ok().body("Pessoa excluída com Sucesso!");
     }
 }
--- src/main/java/com/crud/demo/service/PessoaService.java
+++ src/main/java/com/crud/demo/service/PessoaService.java
@@ -9,6 +9,9 @@ import com.crud.demo.model.Pessoa;
 import com.crud.demo.repository.PessoaRepository;
 import com.crud.demo.service.mapper.PessoaMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -23,11 +26,17 @@ public class PessoaService {
     private final PessoaRepository repository;
     private final PessoaMapper mapper;
 
+    /**
+     * •	Consultar uma pessoa - etapa 3 do desafio
+     * */
     public PessoaDTO obterPorId(int id) {
         Pessoa pessoa = repository.findById(id).orElseThrow(() -> new HttpStatusException("Pessoa com id " + id + " não encontrada", HttpStatus.NOT_FOUND));
         return mapper.toDto(pessoa);
     }
 
+    /**
+     * •	Criar uma Pessoa - etapa  do desafio
+     * */
     public PessoaDTO cadastrar(PessoaCadastroDTO cadastroDTO) {
         //a pessoa precisa se cadastrar com um endereço
         cadastroDTO.getEndereco().setStatusEndereco(StatusEndereco.PRINCIPAL);
@@ -42,21 +51,49 @@ public class PessoaService {
         return pessoaDTO;
     }
 
-    public void adicionarEndereco(int id, EnderecoDTO enderecoDTO) {
+    /**
+     * •	Listar - etapa 4 do desafio
+     * */
+    public Page<PessoaDTO> listar(Pageable pageable) {
+        Page<Pessoa> page = repository.findAll(pageable);
+        List<PessoaDTO> listaDTO = mapper.toDto(repository.findAll());
+        return new PageImpl<>(listaDTO, pageable, page.getTotalElements());
+    }
+
+    /**
+     * •	Criar  endereço para uma Pessoa - etapa 5 do desafio
+     * */
+    public PessoaDTO adicionarEndereco(int id, EnderecoDTO enderecoDTO) {
         PessoaDTO pessoaDTO = obterPorId(id);
         enderecoDTO.setStatusEndereco(StatusEndereco.PRINCIPAL);
         pessoaDTO.getEnderecos().forEach(e -> e.setStatusEndereco(StatusEndereco.SECUNDARIO));
 
         pessoaDTO.getEnderecos().add(enderecoDTO);
-        atualizar(pessoaDTO);
+        return atualizar(pessoaDTO);
     }
 
+    /**
+     * •	Informar endereço principal - etapa 7 do desafio
+     * */
+    public void mudarStatusEndereco(int idPessoa, int idEndereco) {
+        var pessoaDTO = obterPorId(idPessoa);
+        pessoaDTO.getEnderecos().forEach(e -> {
+            e.setStatusEndereco(StatusEndereco.SECUNDARIO);
+            if (e.getId() == idEndereco) e.setStatusEndereco(StatusEndereco.PRINCIPAL);
+        });
+
+        salvar(mapper.toEntity(pessoaDTO));
+    }
+
+    /**
+     * •	Editar Pessoa - etapa 2 do desafio
+     * */
     public PessoaDTO atualizar(PessoaDTO dto) {
         obterPorId(dto.getId()); // garantir que existe
         Pessoa pessoa = salvar(mapper.toEntity(dto));
         return mapper.toDto(pessoa);
     }
-
+    
     public void excluirPorId(int id) {
         repository.deleteById(id);
     }
